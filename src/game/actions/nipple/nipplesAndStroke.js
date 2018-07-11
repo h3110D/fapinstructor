49bbe766aef044ff610026caf90e3cd996e95c29
The1nstructor <theinstructioner@gmail.com>
2018-07-11 16:56:50 +0200

    usage of the newly created functions.

--- src/game/actions/nipple/nipplesAndStroke.js
+++ src/game/actions/nipple/nipplesAndStroke.js
@@ -1,40 +1,38 @@
 import createNotification from "engine/createNotification";
 import {randomStrokeSpeed, setStrokeSpeed,} from "game/utils/strokeSpeed";
-import {RubStrengthEnum, RubStrengthString} from "game/enums/RubStrength"
-import {HandSideEnum, HandSideString} from "game/enums/HandSide";
+import {getRandomRubStrength} from "game/enums/RubStrength"
+import {getRandomLeftOrRight} from "game/enums/HandSide";
 import {getRandomInclusiveInteger} from "utils/math";
 import delay from "utils/delay";
-import {setRandomStrokeStyle, setRandomStrokeStyle_OneHand} from "game/actions/strokeStyle";
+import {setRandomStrokeStyle, setRandomStrokeStyle_OneHand} from "game/enums/StrokeStyle";
 
 /**
- * Task to rub ones nipples while stroking ones cock.
+ * Task to play with ones nipples while stroking ones cock.
  *
- * @since      06.07.2018
+ * @since      11.07.2018
+ * @author     the1nstructor
  *
  * @alias   nipplesAndStroke
  * @memberof actions
  */
 const nipplesAndStroke = async () => {
-    // set intensity
-    const intensity = getRandomInclusiveInteger(0, Object.keys(RubStrengthEnum).length/2);
+  // get Random strength
+  const strength = getRandomRubStrength();
+  const left_or_right = getRandomLeftOrRight();
 
-    const left_or_right = getRandomInclusiveInteger(0, Object.keys(HandSideEnum).length/2);
-    //TODO: always getting "right" so getRandomInclusiveInteger(0, 1) either always returns 0 or Object.keys is wrong calculation.
+  // task duration (= total time in this case)
+  const taskDuration = getRandomInclusiveInteger(10, 25);
+  await setRandomStrokeStyle_OneHand();
+  createNotification(`Use one of your hands to ${strength}play with your ${left_or_right} nipple`, {
+    time: taskDuration * 1000
+  });
 
-    // task duration (= total time in this case)
-    const taskDuration = getRandomInclusiveInteger(10, 25);
+  await delay((taskDuration + 1) * 1000);
 
-    setRandomStrokeStyle_OneHand();
-    createNotification(`Use one of your hands to ${RubStrengthString[intensity]}rub your ${HandSideString[left_or_right]} nipple`, {
-        time: taskDuration * 1000
-    });
-
-    await delay((taskDuration + 1) * 1000);
-
-    setStrokeSpeed(randomStrokeSpeed());
-    setRandomStrokeStyle();
+  setStrokeSpeed(randomStrokeSpeed());
+  await setRandomStrokeStyle();
 
 };
-nipplesAndStroke.label = "Rub Nipples";
+nipplesAndStroke.label = "Nipple and Stroke";
 
 export default nipplesAndStroke;
