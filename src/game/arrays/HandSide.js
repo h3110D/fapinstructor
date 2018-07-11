9dff5b6bb4179b2c1768fcb97bde9e1ae911cb50
The1nstructor <theinstructioner@gmail.com>
2018-07-11 11:11:35 +0200

    chose to rather use right/left. did not think about it right.

---
+++ src/game/arrays/HandSide.js
@@ -0,0 +1,11 @@
+const HandSideStringArray = [
+  {0: "right"},
+  {1: "left"}
+];
+
+const HandSideArray = [
+  {rightHandSide: 0},
+  {leftHandSide: 1}
+];
+
+export { HandSideStringArray, HandSideArray };
