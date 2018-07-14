524d9551c347805a092cffbc50443e98c0feacac
The1nstructor <theinstructioner@gmail.com>
2018-07-15 01:16:23 +0200

    handsOff now does no longer set a random stroking style after completion

--- src/game/actions/handsOff.js
+++ src/game/actions/handsOff.js
@@ -2,7 +2,7 @@ import createNotification from "engine/createNotification";
 import {randomStrokeSpeed, setStrokeSpeed,} from "game/utils/strokeSpeed";
 import {getRandomInclusiveInteger} from "utils/math";
 import delay from "utils/delay";
-import {setRandomStrokeStyle, setStrokeStyleHandsOff} from "game/enums/StrokeStyle";
+import {getCurrentStrokeStyle, setStrokeStyle, setStrokeStyleHandsOff} from "game/enums/StrokeStyle";
 
 /**
  * Task to not touch ones cock
@@ -15,6 +15,7 @@ import {setRandomStrokeStyle, setStrokeStyleHandsOff} from "game/enums/StrokeSty
  */
 const handsOff = async () => {
 
+  const style = getCurrentStrokeStyle();
   // task duration (= total time in this case)
   const taskDuration = getRandomInclusiveInteger(10, 25);
 
@@ -27,7 +28,8 @@ const handsOff = async () => {
   await delay(taskDuration * 1000);
 
   setStrokeSpeed(randomStrokeSpeed());
-  await setRandomStrokeStyle();
+  await setStrokeStyle(style);
+  await delay(1000);
 
 };
 handsOff.label = "Hands Off";
