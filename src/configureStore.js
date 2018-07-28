f9ebab698b364182834e6274d7ade9366beee7b0
thefapinstructor <thefapinstructor@gmail.com>
2018-07-28 15:14:04 -0400

    removed duplicate key

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,12 +3,12 @@ import { GripStrengthEnum } from "game/enums/GripStrength";
 
 const defaultConfig = {
   version: 2,
-  tumblrId: "fapstergifs, allcowgirl, mar-cuss-blowjob-gifs, pornonmotion[blowjob,cumshot]",
+  tumblrId:
+    "fapstergifs, allcowgirl, mar-cuss-blowjob-gifs, pornonmotion[blowjob,cumshot]",
   failedIds: [],
   gifs: true,
   redditId: "60fpsporn, porninaminute",
-  gifs: false,
-  pictures: false,
+  pictures: true,
   videos: true,
   slideDuration: 10, // sec
   enableVoice: true,
