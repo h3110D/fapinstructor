701e78abcaf894c6e47c76968835cd93f5bd4d0c
LenAnderson <anderson.len@outlook.com>
2018-11-17 13:47:45 +0100

    remove console output

--- src/api/fetchVReddit.js
+++ src/api/fetchVReddit.js
@@ -1,6 +1,5 @@
 const fetchVReddit = async (post) => {
     let media = post.media ? post.media.reddit_video : (post.secure_media) ? post.secure_media.reddit_video : null;
-    console.log('media', media, post);
 
     if (media) {
         return media.fallback_url;
