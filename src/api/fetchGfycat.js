d36b913b47e2d71fe392adc9c14a1413ed4c4f0c
LenAnderson <anderson.len@outlook.com>
2018-12-02 00:04:34 +0100

    remove console output

--- src/api/fetchGfycat.js
+++ src/api/fetchGfycat.js
@@ -1,17 +1,13 @@
 const fetchGfycat = async (url) => {
-    console.log('fetchGfycat', url);
     return new Promise((resolve, reject)=>{
         let id = url.replace(/^.*gfycat.com\/([^/?]+)(?:[/?].*)?$/, '$1');
         let xhr = new XMLHttpRequest();
         xhr.open('GET', `https://api.gfycat.com/v1/gfycats/${id}`, true);
         xhr.addEventListener('load', ()=>{
-            console.log('loaded', url);
             try {
                 let data = JSON.parse(xhr.responseText);
                 resolve(data.gfyItem.webmUrl);
-                console.log('resolved', url);
             } catch (ex) {
-                console.warn(ex, url, xhr);
                 resolve();
             }
         });
